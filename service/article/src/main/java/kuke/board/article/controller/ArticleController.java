acb7e282e78b2deafd3a08f39280cd5e224e7a1c
sariiiiiiii <abdf13423@naver.com>
2025-04-15 17:38:16 +0900

    Article API 추가

---
+++ service/article/src/main/java/kuke/board/article/controller/ArticleController.java
@@ -0,0 +1,36 @@
+package kuke.board.article.controller;
+
+import kuke.board.article.service.ArticleService;
+import kuke.board.article.service.request.ArticleCreateRequest;
+import kuke.board.article.service.request.ArticleUpdateRequest;
+import kuke.board.article.service.response.ArticleResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @GetMapping("/v1/articles/{articleId}")
+    public ArticleResponse read(@PathVariable Long articleId) {
+        return articleService.read(articleId);
+    }
+
+    @PostMapping("/v1/articles")
+    public ArticleResponse create(@RequestBody final ArticleCreateRequest request) {
+        return articleService.create(request);
+    }
+
+    @PutMapping("/v1/articles/{articleId}")
+    public ArticleResponse update(@PathVariable final Long articleId, @RequestBody final ArticleUpdateRequest request) {
+        return articleService.update(articleId, request);
+    }
+
+    @DeleteMapping("/v1/articles/{articleId}")
+    public void delete(@PathVariable final Long articleId) {
+        articleService.delete(articleId);
+    }
+
+}
